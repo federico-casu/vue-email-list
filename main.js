48030ccd56c79a529ac3bdc2279b12b2619a1a66
federico-casu <federico.casu1101@gmail.com>
2024-02-05 15:48:23 +0100

    feat: generated 10 email addresses and pushed them into rndm_mails array

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            rndm_mails: []
         }
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (result) => {
+                this.rndm_mails.push(result.data.response)
+            } )            
+        }
+
+        console.log(this.rndm_mails)
     },
     methods: {
         
